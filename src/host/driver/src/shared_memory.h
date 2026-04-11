0c576a008f8b6c45c981726ec31b04dd5169d54f
Blockmann2K <blockmann2k@gmail.com>
2026-04-11 23:45:43 +0200

    feat(shared-mem): Implement Shared Memory Pose Polling
    - Add Shared Memory Connection and Pose Polling in `hmd_device_driver.cpp`
    - Create `shared_memory.h` With Memory Mapping and Pose Reading Functions
    - Update `vr_pose_shared.h` With `VRPoseShared` and `VRPoseSnapshot` Structs
    - Remove Temporary Test Files (`_temporary.cpp`, `_temporary.exe`, `_temporary.obj`)
    - Modify `main.rs` To Update Quaternion Values for Simple Y (Yaw) Rotating

--- src/host/driver/src/shared_memory.h
+++ src/host/driver/src/shared_memory.h
@@ -1,14 +1,20 @@
 // Copyright (c) 2026 MurlokVR Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-#pragma once
+//-----------------------------------------------------------------------------
+// Dependencies
+//-----------------------------------------------------------------------------
+#pragma once // Only Include This Current Header File Once in a Compilation
 
-#include <Windows.h>
-#include <iostream>
+#include <Windows.h> // Windows API
+#include <iostream>  // Provides 'cerr' & 'cout'
 
-#include "vr_pose_shared.h"
+#include "vr_pose_shared.h" // Provides 'VRPoseShared' & 'VRPoseSnapshot'
 
-inline int hello_world() {
+//-----------------------------------------------------------------------------
+// Open Shared Memory & Pose Polling
+//-----------------------------------------------------------------------------
+inline LPVOID OpenVRPoseMemory() {
   HANDLE hHandle =
       OpenFileMappingW(FILE_MAP_READ, FALSE, L"Global\\MurlokVR_Shared_Memory");
 
@@ -16,7 +22,7 @@ inline int hello_world() {
     std::cerr << "DEBUG: OpenFileMapping Failed! - Error: " << GetLastError()
               << "\n";
 
-    return 1;
+    return nullptr;
   }
 
   LPVOID pView = MapViewOfFile(hHandle, FILE_MAP_READ, 0, 0, 0);
@@ -25,28 +31,33 @@ inline int hello_world() {
     std::cerr << "DEBUG: MapViewOfFile Failed! - Error: " << GetLastError()
               << "\n";
 
-    return 1;
+    return nullptr;
   }
 
-  const VRPoseShared *pose = reinterpret_cast<const VRPoseShared *>(pView);
+  return pView;
+}
 
-  VRPoseSnapshot vr_pose_shared_snapshot;
+inline VRPoseSnapshot PollVRPoseSnapshot(const VRPoseShared *PoseShared) {
+  VRPoseSnapshot PoseSnapshot;
 
-  for (int i = 0; i < 10; i++) {
-    vr_pose_shared_snapshot = read_pose(pose);
+  while (true) {
+    uint32_t unSeqLockBefore =
+        PoseShared->sequence_counter.load(std::memory_order_acquire);
 
-    float x = vr_pose_shared_snapshot.quaternion_x;
-    std::cout << "Quaternion X: " << x << "\n";
+    PoseSnapshot.device_status = PoseShared->device_status;
+    PoseSnapshot.heartbeat_timestamp = PoseShared->heartbeat_timestamp;
+    PoseSnapshot.quaternion_x = PoseShared->quaternion_x;
+    PoseSnapshot.quaternion_y = PoseShared->quaternion_y;
+    PoseSnapshot.quaternion_z = PoseShared->quaternion_z;
+    PoseSnapshot.quaternion_w = PoseShared->quaternion_w;
 
-    float y = vr_pose_shared_snapshot.quaternion_y;
-    std::cout << "Quaternion Y: " << y << "\n";
+    uint32_t unSeqLockAfter =
+        PoseShared->sequence_counter.load(std::memory_order_acquire);
 
-    float z = vr_pose_shared_snapshot.quaternion_z;
-    std::cout << "Quaternion Z: " << z << "\n";
-
-    float w = vr_pose_shared_snapshot.quaternion_w;
-    std::cout << "Quaternion W: " << w << "\n";
+    if (unSeqLockBefore == unSeqLockAfter) {
+      break;
+    }
   }
 
-  return 0;
+  return PoseSnapshot;
 }
